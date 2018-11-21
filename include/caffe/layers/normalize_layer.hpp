ed621fd5387de61b3b323c77c990f05cc032a7f1
hezhixiang <247281309@qq.com>
2018-11-21 13:42:25 +0800

    修改为新的L2-Norm层，保持与NCNN的一致

--- include/caffe/layers/normalize_layer.hpp
+++ include/caffe/layers/normalize_layer.hpp
@@ -8,36 +8,45 @@
 #include "caffe/proto/caffe.pb.h"
 
 namespace caffe {
+
     /**
-    * @brief Normalizes the input to be 0 - 1 range.
+    * @brief Normalizes the input to have L_p norm of 1 with scale learnable.
     *
-    * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+    * TODO(weiliu89): thorough documentation for Forward, Backward, and proto params.
     */
     template <typename Dtype>
     class NormalizeLayer : public Layer<Dtype> {
     public:
         explicit NormalizeLayer(const LayerParameter& param)
             : Layer<Dtype>(param) {}
-        virtual void Reshape(const vector< Blob<Dtype>* > &bottom,
-            const vector< Blob<Dtype>* > &top);
+        virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+            const vector<Blob<Dtype>*>& top);
+        virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+            const vector<Blob<Dtype>*>& top);
 
         virtual inline const char* type() const { return "Normalize"; }
         virtual inline int ExactNumBottomBlobs() const { return 1; }
         virtual inline int ExactNumTopBlobs() const { return 1; }
 
     protected:
-        virtual void Forward_cpu(const vector< Blob<Dtype>* > &bottom,
-            const vector< Blob<Dtype>* > &top);
-        virtual void Forward_gpu(const vector< Blob<Dtype>* > &bottom,
-            const vector< Blob<Dtype>* > &top);
-        virtual void Backward_cpu(const vector< Blob<Dtype>* > &top,
-            const vector<bool> &propagate_down, const vector< Blob<Dtype>* > &bottom);
-        virtual void Backward_gpu(const vector< Blob<Dtype>* > &top,
-            const vector<bool> &propagate_down, const vector< Blob<Dtype>* > &bottom);
+        virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+            const vector<Blob<Dtype>*>& top);
+        virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+            const vector<Blob<Dtype>*>& top);
+        virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+            const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+        virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+            const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
-        Blob<Dtype> squared_;
-        static const float EPS;	// = 0.000001f;
+        Blob<Dtype> norm_;
+        Blob<Dtype> sum_channel_multiplier_, sum_spatial_multiplier_;
+        Blob<Dtype> buffer_, buffer_channel_, buffer_spatial_;
+        bool across_spatial_;
+        bool channel_shared_;
+        Dtype eps_;
     };
+
 }  // namespace caffe
 
-#endif  // CAFFE_NORMALIZE_LAYER_HPP_
+#endif  // CAFFE_MVN_LAYER_HPP_
+
