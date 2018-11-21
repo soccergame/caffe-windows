09bd0ca3ea89bfb2332a77861530db281420db3d
hezhixiang <247281309@qq.com>
2018-11-21 13:00:10 +0800

    修改normliaze层

--- include/caffe/layers/normalize_layer.hpp
+++ include/caffe/layers/normalize_layer.hpp
@@ -1,7 +1,6 @@
 #ifndef CAFFE_NORMALIZE_LAYER_HPP_
 #define CAFFE_NORMALIZE_LAYER_HPP_
 
-#include <utility>
 #include <vector>
 
 #include "caffe/blob.hpp"
@@ -9,40 +8,36 @@
 #include "caffe/proto/caffe.pb.h"
 
 namespace caffe {
-  /**
-  * @brief Normalizes input.
-  */
-  template <typename Dtype>
-  class NormalizeLayer : public Layer<Dtype> {
-  public:
-    explicit NormalizeLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-    virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-                            const vector<Blob<Dtype>*>& top);
-    virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-                         const vector<Blob<Dtype>*>& top);
+    /**
+    * @brief Normalizes the input to be 0 - 1 range.
+    *
+    * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+    */
+    template <typename Dtype>
+    class NormalizeLayer : public Layer<Dtype> {
+    public:
+        explicit NormalizeLayer(const LayerParameter& param)
+            : Layer<Dtype>(param) {}
+        virtual void Reshape(const vector< Blob<Dtype>* > &bottom,
+            const vector< Blob<Dtype>* > &top);
 
-    virtual inline const char* type() const { return "Normalize"; }
-    virtual inline int ExactNumBottomBlobs() const { return 1; }
-    virtual inline int MinTopBlobs() const { return 1; }
-    virtual inline int MaxTopBlobs() const { return 2; }
+        virtual inline const char* type() const { return "Normalize"; }
+        virtual inline int ExactNumBottomBlobs() const { return 1; }
+        virtual inline int ExactNumTopBlobs() const { return 1; }
 
-  protected:
-    virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-                             const vector<Blob<Dtype>*>& top);
-    virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-                             const vector<Blob<Dtype>*>& top);
-    virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-                              const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-                              const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    protected:
+        virtual void Forward_cpu(const vector< Blob<Dtype>* > &bottom,
+            const vector< Blob<Dtype>* > &top);
+        virtual void Forward_gpu(const vector< Blob<Dtype>* > &bottom,
+            const vector< Blob<Dtype>* > &top);
+        virtual void Backward_cpu(const vector< Blob<Dtype>* > &top,
+            const vector<bool> &propagate_down, const vector< Blob<Dtype>* > &bottom);
+        virtual void Backward_gpu(const vector< Blob<Dtype>* > &top,
+            const vector<bool> &propagate_down, const vector< Blob<Dtype>* > &bottom);
 
-    Blob<Dtype> sum_multiplier_, squared_, norm_;
-    std::string normalize_type_;
-    bool fix_gradient_;
-    bool bp_norm_;
-  };
-
-}
+        Blob<Dtype> squared_;
+        static const float EPS;	// = 0.000001f;
+    };
+}  // namespace caffe
 
 #endif  // CAFFE_NORMALIZE_LAYER_HPP_
